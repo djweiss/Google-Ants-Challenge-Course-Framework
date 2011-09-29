4ad0c667e544ed092a68a528f1bb0114ccd304f1
Ben Sapp <benjamin.sapp@gmail.com>
2011-09-29 14:17:16 -0400

    fixed run game to handle arbitrary # of bots

--- base/rungame.py
+++ base/rungame.py
@@ -1,5 +1,5 @@
 import sys
-
+from optparse import OptionParser
 
 #Main section allowing for dual use either with the standard engine
 # or the LocalEngine. If no arguments are given, it calls
@@ -8,29 +8,35 @@ import sys
 # LocalEngine and creates two instances of GreedyBot to play against
 # one another.
 if __name__ == '__main__':
-  # From Ants distribution: use psyco to speed up all code.
-  print sys.argv
-  try:
-    import psyco
-    psyco.full()
-  except ImportError:
-    pass
+    # From Ants distribution: use psyco to speed up all code.
+    print sys.argv
+    try:
+        import psyco
+        psyco.full()
+    except ImportError:
+        pass
 
-  try:
-    #-3 here is to cut of .py, which is not used in imports
-    Red = __import__(sys.argv[1][:-3]).BOT
-    if len(sys.argv) > 2: # Run LocalEngine version
-      Blue = __import__(sys.argv[2][:-3]).BOT
-      from src.localengine import LocalEngine
-      engine = LocalEngine()
-      engine.AddBot(Red(engine.GetWorld()))
-      engine.AddBot(Blue(engine.GetWorld()))
-      del sys.argv[1]
-      del sys.argv[1]
-      engine.Run(sys.argv)
-    else: # Run as stand-alone process
-      bot = Red(AntWorld())
-      bot._run()
-  except KeyboardInterrupt:
-      print('ctrl-c, leaving ...')
+    try:
+        #-3 here is to cut of .py, which is not used in imports
+
+        botlist = [s for s in sys.argv[1:] if s[-3:] == '.py']
+        arglist = [sys.argv[0]]+[s for s in sys.argv[1:] if s[-3:] != '.py']
+        bots = [__import__(botstr[:-3]).BOT for botstr in botlist]
+        
+        if len(sys.argv) > 2: # Run LocalEngine version
+      
+            from src.localengine import LocalEngine
+            engine = LocalEngine()
+
+            for B in bots:
+                engine.AddBot(B(engine.GetWorld()))
+            
+            engine.Run(arglist)
+      
+        else: # Run as stand-alone process
+            bot = bots[0](AntWorld())
+            bot._run()
+          
+    except KeyboardInterrupt:
+        print('ctrl-c, leaving ...')
 
